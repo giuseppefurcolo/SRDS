eb9c39580bd708de2989909156f6980f7066b1b4
giuseppefurcolo <me@giuseppefurcolo.com>
2023-11-09 11:40:22 -0400

    footy

--- copy.js
+++ copy.js
@@ -1,6 +1,6 @@
 const interiors = [
     '0', 
-    'Gut renovation of coworking space in the hart of Roma, Italy', 
+    'Gut renovation of coworking space in the heart of Roma, Italy', 
     'world', 
     '',
     '',
